bdcd449ea106f89eb5cdb4608385fd4185a1ab50
vysoforlife <95134184+vysoforlife@users.noreply.github.com>
2023-08-25 18:52:56 +0400

    Update rgc.cuh

--- src/cud/rgc.cuh
+++ src/cud/rgc.cuh
@@ -4,6 +4,10 @@
 
 #ifndef TEST1_RGC_CUH
 #define TEST1_RGC_CUH
+typedef struct {
+    int rgcArrayHeight, RGCcount, fps, *xWidths, *yMatches;
+} vid_rgc_params;
+
 void saxpy();
 int rgcSpikers (queue<float**> *rihq_l, queue<float**> *rihq_r, vid2rgcParams *v2rp, queue<RGC**> *rgcdetails, mutex &rgcMut, condition_variable &rgcCond);
 
